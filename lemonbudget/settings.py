e842e141338ceb7aa718013e7ee54db212133010
Brad Lee <dynamicthrashing@gmail.com>
2022-11-30 21:07:48 -0800

    Add `custom_user_model` app to settings.py

--- lemonbudget/settings.py
+++ lemonbudget/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "custom_user_model.apps.CustomUserModelConfig",
 ]
 
 MIDDLEWARE = [
