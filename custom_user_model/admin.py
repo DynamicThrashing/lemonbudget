70b5e7bcd8900cd6261472e98e8399dbdf0a0756
Brad Lee <dynamicthrashing@gmail.com>
2022-11-30 23:27:14 -0800

    Added Groups and Permissions to the CustomUser admin registration

--- custom_user_model/admin.py
+++ custom_user_model/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from .models import CustomUser
 
@@ -18,9 +19,24 @@ class CustomUserAdmin(UserAdmin):
         "is_staff",
         "is_active",
     )
+    # fieldsets = (
+    #     (None, {"fields": ("email", "password")}),
+    #     ("Permissions", {"fields": ("is_staff", "is_active")}),
+    # )
     fieldsets = (
         (None, {"fields": ("email", "password")}),
-        ("Permissions", {"fields": ("is_staff", "is_active")}),
+        (
+            _("Roles"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                )
+            },
+        ),
+        (_("Permissions"), {"fields": ("groups", "user_permissions")}),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
     )
     add_fieldsets = (
         (
